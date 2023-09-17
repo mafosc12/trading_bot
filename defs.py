0ab8b12a8131f95bd6651391a50c3bb09713ea99
mafosc12 <145271986+mafosc12@users.noreply.github.com>
2023-09-17 15:23:10 +0100

    Update defs.py

--- defs.py
+++ defs.py
@@ -1,3 +1,3 @@
-API_KEY = "824e51572c21768b8e0067f9d048dabb-ab1694419da5d3debf39a7d7ce0c422d"
-ACCOUNT_ID = "101-004-26394867-001"
+API_KEY = 
+ACCOUNT_ID = 
 OANDA_URL = "https://api-fxpractice.oanda.com/v3"
